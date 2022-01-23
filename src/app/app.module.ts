60081d7c9600ccbc0687d6c96d0a1e4354a29722
Max <maximiliano@omotor.com.br>
2022-01-23 16:26:45 -0300

    Criados os  routes, shared, core e layout para compor a estrutura da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { RoutesModule } from './routes/routes.module';
+import { LayoutModule } from './layout/layout.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    RoutesModule,
+    LayoutModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
